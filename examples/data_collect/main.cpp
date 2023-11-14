4ffc39203a9a61fd4d35f74868ee9b9237a45ead
Alexandru Lie <Alexandru.Lie@analog.com>
2023-11-14 14:05:11 +0200

    Added full-frame option
    Signed-off-by: Alexandru Lie <Alexandru.Lie@analog.com>

--- examples/data_collect/main.cpp
+++ examples/data_collect/main.cpp
@@ -81,7 +81,7 @@ static const char kUsagePublic[] =
       --ccb <FILE>       The path to store CCB content
       --ip <ip>          Camera IP
       --fw <firmware>    Adsd3500 fw file
-      --ft <frameType>   FrameType of saved image (depth/ir/conf) [default: depth]
+      --ft <frameType>   FrameType of saved image (depth/ir/conf/full-frame) [default: depth]
 
     Note: --m argument supports both index and string (0/sr-native) 
 
@@ -256,11 +256,12 @@ int main(int argc, char *argv[]) {
     frame_type = command_map["-ft"].value;
 
     if (frame_type.length() <= 0 &&
-        (!frame_type.compare(std::string("depth")) ||
+        (!frame_type.compare(std::string("full-frame")) ||
+         !frame_type.compare(std::string("depth")) ||
          !frame_type.compare(std::string("ir")) ||
          !frame_type.compare(std::string("conf")))) {
         LOG(ERROR) << "Error parsing frame_type (-ft/--ft) from command line!"
-                   << "\n Possible values: depth, ir, conf"
+                   << "\n Possible values: depth, ir, conf, full-frame"
                    << "\n Please check help menu";
         return 0;
     }
@@ -370,7 +371,7 @@ int main(int argc, char *argv[]) {
     // pcm-native contains ir only
     if (frame_type != "ir" && modeName == "pcm-native") {
         LOG(ERROR) << modeName
-                   << " mode doesn't contain depth/conf data, setting --ft "
+                   << " mode doesn't contain depth/conf/full-frame data, setting --ft "
                       "(frameType) to ir.";
         frame_type = "ir";
     }
@@ -429,13 +430,13 @@ int main(int argc, char *argv[]) {
     // Wait until the warmup time is finished
     if (warmup_time > 0) {
         do {
-            uint16_t *pRawFrame;
+            uint16_t *pFrame;
             status = camera->requestFrame(&frame);
             if (status != Status::OK) {
                 LOG(ERROR) << "Could not request frame!";
                 return 0;
             }
-            status = frame.getData(frame_type, &pRawFrame);
+            status = frame.getData(frame_type, &pFrame);
             if (status != Status::OK) {
                 LOG(ERROR) << "Could not get " << frame_type
                            << " frame type data!";
@@ -498,6 +499,11 @@ int main(int argc, char *argv[]) {
             }
 
             frame_size = sizeof(float) * height * width;
+        } else if (frame_type == "full-frame") {
+            FrameDataDetails frameDetails;
+            frame.getDataDetails("frameData", frameDetails);
+            frame_size =
+                sizeof(uint16_t) * frameDetails.height * frameDetails.width;
         } else {
             LOG(WARNING) << "Can't recognize frame data type!";
         }
@@ -510,17 +516,31 @@ int main(int argc, char *argv[]) {
             return 0;
         }
 
-        uint16_t *pData;
-        status = frame.getData(frame_type, &pData);
-        if (status != Status::OK) {
-            LOG(ERROR) << "Could not get frame type data!";
-            return 0;
+        if (frame_type == "full-frame") {
+            uint16_t *pData;
+            status = frame.getData("frameData", &pData);
+            if (status != Status::OK) {
+                LOG(ERROR) << "Could not get frame type data!";
+                return 0;
+            }
+            if (!pData) {
+                LOG(ERROR) << "no memory allocated in frame";
+                return 0;
+            }
+            memcpy(frameBuffer, (uint8_t *)pData, frame_size);
+        } else {
+            uint16_t *pData;
+            status = frame.getData(frame_type, &pData);
+            if (status != Status::OK) {
+                LOG(ERROR) << "Could not get frame type data!";
+                return 0;
+            }
+            if (!pData) {
+                LOG(ERROR) << "no memory allocated in frame";
+                return 0;
+            }
+            memcpy(frameBuffer, (uint8_t *)pData, frame_size);
         }
-        if (!pData) {
-            LOG(ERROR) << "no memory allocated in frame";
-            return 0;
-        }
-        memcpy(frameBuffer, (uint8_t *)pData, frame_size);
 
         uint32_t header_data_size = EMBED_HDR_LENGTH;
         headerBuffer = new uint8_t[header_data_size];
